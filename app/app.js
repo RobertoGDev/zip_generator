cb52a8addb4caf85071fb5748aac5a67cba0b714
Roberto <rob.vicalvaro@gmail.com>
2018-06-10 21:15:04 +0200

    Arreglando catch y posibilidad de que no quiera hacer zip

--- app/app.js
+++ app/app.js
@@ -1,15 +1,20 @@
 //Iniciamos dependencias
 const fs = require('fs');
 const inquirer = require('inquirer');
-const archiver = require('archiver').Zip;
+const archiver = require('archiver');
 
-//Creamos las questions del prompt
-let questions = [{
+//Question si quiere hacer el zip, para continuar o no
+let wantToZip = [
+    {
         message: '¿Desea crear un archivo zip?',
         type: 'confirm',
         default: false,
         name: 'setZip'
-    },
+    }
+];
+
+//Creamos las questions del prompt
+let questions = [
     {
         message: 'Seleccione los archivos a incluir',
         type: 'checkbox',
@@ -26,7 +31,7 @@ let questions = [{
         ]
     },
     {
-        message: 'Finalmente dé un nombre al archivo zip',
+        message: 'Asigne un nombre al archivo zip de salida',
         type: 'input',
         name: 'outputfile',
     }
@@ -36,63 +41,63 @@ let questions = [{
 //answers.outputFile
 //answers.fileToInclude
 
+inquirer.prompt(wantToZip).then(continueExecution => {
 
-inquirer.prompt(questions).then(answers => {
-
-    if (answers.setZip) {
-        var dir = './zip_folder';
-
-        if (!fs.existsSync(dir)) {
-            fs.mkdirSync(dir);
-        }
-
-        let output = fs.createWriteStream(dir + '/' + answers.outputfile + '.zip');
-        let archive = archiver('zip', {
-            zlib: {
-                level: 9
-            } // Sets the compression level.
-        });
-
-        output.on('close', function () {
-            console.log(archive.pointer() + ' total bytes');
-            console.log('el archivador se ha finalizado y el descriptor del archivo de salida se ha cerrado.');
-        });
-
-        output.on('end', function () {
-            console.log('Archivos compilados en zip');
-        });
-
-        // good practice to catch warnings (ie stat failures and other non-blocking errors)
-        archive.on('warning', function (err) {
-            if (err.code === 'ENOENT') {
-                // log warning
-            } else {
-                // throw error
-                throw err;
-            }
-        });
-
-        // good practice to catch this error explicitly
-        archive.on('error', function (err) {
-            throw err;
-        });
-
-        archive.pipe(output);
-
-        answers.filesToInclude.forEach(file => {
-            archive.append(fs.createReadStream('../files_to_zip/' + file), {
-                name: file
-            });
-        });
-
-        archive.finalize();
+    if (continueExecution.setZip) {
         
+        inquirer.prompt(questions).then(answers => {
+
+            let dir = './zip_folder';
+
+            if (!fs.existsSync(dir)) {
+                fs.mkdirSync(dir);
+            }
+
+            let output = fs.createWriteStream(dir + '/' + answers.outputfile + '.zip');
+            let archive = archiver('zip', {
+                zlib: {
+                    level: 9
+                } // Sets the compression level.
+            });
+
+            output.on('close', function () {
+                console.log(archive.pointer() + ' total bytes');
+                console.log('el archivador se ha finalizado y el descriptor del archivo de salida se ha cerrado.');
+            });
+
+            output.on('end', function () {
+                console.log('Archivos compilados en zip');
+            });
+
+            // good practice to catch warnings (ie stat failures and other non-blocking errors)
+            archive.on('warning', function (err) {
+                if (err.code === 'ENOENT') {
+                    // log warning
+                } else {
+                    // throw error
+                    throw err;
+                }
+            });
+
+            // good practice to catch this error explicitly
+            archive.on('error', function (err) {
+                throw err;
+            });
+
+            archive.pipe(output);
+
+            answers.filesToInclude.forEach(file => {
+                archive.append(fs.createReadStream('./files_to_zip/' + file), {
+                    name: file
+                });
+            });
+
+            archive.finalize();
+
+        }).catch((error) => {
+            console.log('Ha ocurrido un error: '+error);
+        });       
     } else {
-        console.log('Bye bye!');
-        return
+        console.log('Saliendo del sistema...');
     }
-
-    createZip(answers);
 });
-
-
